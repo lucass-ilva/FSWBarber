9e87fde5d5768a2d9013849fd3abd2311be2e0c1
Lucas Silva <lukatoninho1610@outlook.com>
2024-08-05 22:46:51 -0300

    feat: add Header componente and banner

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,40 @@
 // não consegue adicionar iteratividade exemplo onClick, ou UseState (variavel interativa) pois precisa de manipulação de DOM JS
 // ao escrever "use client" o componente vira um Client Component
 
-"use client";
+"use client"
 
-import { Button } from "./_components/ui/button";
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
-const Home = () => {  
-  return <Button>Test</Button>
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Lucas!</h2>
+        <p>Segunda-feira, 05 de agosto.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default Home;
+export default Home
